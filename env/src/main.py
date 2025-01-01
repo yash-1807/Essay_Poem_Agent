7be41b9f8a5254ba14d521bb5335d856ac5b67fc
yash-1807 <128221236+yash-1807@users.noreply.github.com>
2025-01-01 15:06:29 +0530

    Initial commit using phidata

---
+++ env/src/main.py
@@ -0,0 +1,13 @@
+from phi.agent import Agent 
+from phi.environment import Environment
+from phi.agent import Agent
+
+
+
+
+essay_writting_agent = Agent(
+    # Add functions or Toolkits
+    tools=[],
+    # Show tool calls in the Agent response
+    show_tool_calls=True
+)
